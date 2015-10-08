276301ac3a4ef397e5f15d49f3a91db47630029e
Alvaro del Castillo <acs@bitergia.com>
2015-10-08 16:40:45 +0200

    Added orgs and projects data to approvals. Added loading of Sorting Hat data to ES.

--- utils/gerrit2el.py
+++ utils/gerrit2el.py
@@ -22,18 +22,65 @@
 # Authors:
 #   Alvaro del Castillo San Felix <acs@bitergia.com>
 #
-#
+# TODO: Just a playing script yet.
 
 import json
 import logging
+import MySQLdb
 import os
 import subprocess
 import time
 import urllib2
 
+# https://github.com/jgbarah/Grimoire-demo/blob/master/grimoire-ng-data.py#L338
+class Database:
+    """To work with a database (likely including several schemas).
+    """
+
+    def __init__ (self, user, passwd, host, port, scrdb, shdb, prjdb):
+        self.user = user
+        self.passwd = passwd
+        self.host = host
+        self.port = port
+        self.scrdb = scrdb
+        self.shdb = shdb
+        self.prjdb = prjdb
+        self.db, self.cursor = self._connect()
+
+    def _connect(self):
+        """Connect to the MySQL database.
+        """
+
+        try:
+            db = MySQLdb.connect(user = self.user, passwd = self.passwd,
+                                 host = self.host, port = self.port,
+                                 db = self.shdb,
+                                 use_unicode = True)
+            return db, db.cursor()
+        except:
+            logging.error("Database connection error")
+            raise
+
+    def execute(self, query):
+        """Execute an SQL query with the corresponding database.
+        The query can be "templated" with {scm_db} and {sh_db}.
+        """
+
+        # sql = query.format(scm_db = self.scmdb,
+        #                   sh_db = self.shdb,
+        #                   prj_db = self.prjdb)
+
+        results = int (self.cursor.execute(query))
+        if results > 0:
+            result1 = self.cursor.fetchall()
+            return result1
+        else:
+            return []
+
 
 def fix_review_dates(item):
-    # Convert dates so ES detect them
+    """ Convert dates so ES detect them """
+
     for date_field in ['timestamp','createdOn','lastUpdated']:
         if date_field in item.keys():
             date_ts = item[date_field]
@@ -56,7 +103,8 @@ def fix_review_dates(item):
                                                  time.localtime(cdate_ts))
 
 def get_projects():
-    # First we need all projects
+    """ Get all projects in gerrit """
+
     cache_file = "gerrit-projects_cache.json"
     cache_file = os.path.join(cache_dir, cache_file)
 
@@ -76,16 +124,117 @@ def get_projects():
 
     return projects
 
+def get_organization(email):
+    """ Get in the most efficient way the organization for an email """
+
+    org = "Unknown"
+    try:
+        org = email2org[email]
+    except:
+        # logging.info("Can't find org for " + email)
+        pass
+
+    return org
+
 def fetch_events(review):
     """ Fetch in ES patches and comments (events) as documents """
+
+    elasticsearch_type = "reviews_events"
+    url = elasticsearch_url + "/"+elasticsearch_index
+    url_type = url + "/" + elasticsearch_type
+
+    reviews_events_map = """
+    {
+        "properties": {
+           "approval_email": {
+              "type": "string",
+              "index":"not_analyzed"
+           },
+           "approval_grantedOn": {
+              "type": "date",
+              "format": "dateOptionalTime"
+           },
+           "approval_organization": {
+              "type": "string",
+              "index":"not_analyzed"
+           },
+           "approval_type": {
+              "type": "string",
+              "index":"not_analyzed"
+           },
+           "approval_username": {
+              "type": "string"
+           },
+           "approval_value": {
+              "type": "long"
+           },
+           "patchSet_createdOn": {
+              "type": "date",
+              "format": "dateOptionalTime"
+           },
+           "patchSet_email": {
+              "type": "string",
+              "index":"not_analyzed"
+           },
+           "patchSet_id": {
+              "type": "string"
+           },
+           "patchSet_organization": {
+              "type": "string",
+              "index":"not_analyzed"
+           },
+           "review_branch": {
+              "type": "string",
+              "index":"not_analyzed"
+           },
+           "review_createdOn": {
+              "type": "date",
+              "format": "dateOptionalTime"
+           },
+           "review_email": {
+              "type": "string",
+              "index":"not_analyzed"
+           },
+           "review_id": {
+              "type": "string"
+           },
+           "review_organization": {
+              "type": "string",
+              "index":"not_analyzed"
+           },
+           "review_project": {
+              "type": "string",
+              "index":"not_analyzed"
+           },
+           "review_status": {
+              "type": "string",
+              "index":"not_analyzed"
+           }
+        }
+    }
+    """
+    # Create mappings
+    url_map = url_type+"/_mapping"
+    request = urllib2.Request(url_map, data=reviews_events_map)
+    request.get_method = lambda: 'PUT'
+    opener.open(request)
+
 
     bulk_json = ""  # Bulk JSON to be feeded in ES
 
     # Review fields included in all events
     bulk_json_review  = '"review_id":"%s",' % review['id']
     bulk_json_review += '"review_createdOn":"%s",' % review['createdOn']
-    bulk_json_review += '"review_email":"%s",' % review['owner']['email']
-    bulk_json_review += '"review_status":"%s"' % review['status']
+    if 'owner' in review and 'email' in review['owner']:
+        email = review['owner']['email']
+        bulk_json_review += '"review_email":"%s",' % email
+        bulk_json_review += '"review_organization":"%s",' % get_organization(email)
+    else:
+        bulk_json_review += '"review_email":null,'
+        bulk_json_review += '"review_organization":null,'
+    bulk_json_review += '"review_status":"%s",' % review['status']
+    bulk_json_review += '"review_project":"%s",' % review['project']
+    bulk_json_review += '"review_branch":"%s"' % review['branch']
     # bulk_json_review += '"review_subject":"%s"' % review['subject']
     # bulk_json_review += '"review_topic":"%s"' % review['topic']
 
@@ -94,15 +243,19 @@ def fetch_events(review):
         bulk_json_patch  = '"patchSet_id":"%s",' % patch['number']
         bulk_json_patch += '"patchSet_createdOn":"%s",' % patch['createdOn']
         if 'author' in patch and 'email' in patch['author']:
-            bulk_json_patch += '"patchSet_email":"%s"' % patch['author']['email']
+            email = patch['author']['email']
+            bulk_json_patch += '"patchSet_email":"%s",' % email
+            bulk_json_patch += '"patchSet_organization":"%s"' % get_organization(email)
         else:
-            bulk_json_patch += '"patchSet_email":None'
+            bulk_json_patch += '"patchSet_email":null,'
+            bulk_json_patch += '"patchSet_organization":null'
 
         app_count = 0  # Approval counter for unique id
         if 'approvals' not in patch:
-            bulk_json_ap  = '"approval_type":None,'
-            bulk_json_ap += '"approval_value":None,'
-            bulk_json_ap += '"approval_email":None'
+            bulk_json_ap  = '"approval_type":null,'
+            bulk_json_ap += '"approval_value":null,'
+            bulk_json_ap += '"approval_email":null,'
+            bulk_json_ap += '"approval_organization":null'
 
             bulk_json_event = '{%s,%s,%s}' % (bulk_json_review,
                                               bulk_json_patch, bulk_json_ap)
@@ -118,12 +271,14 @@ def fetch_events(review):
                 bulk_json_ap += '"approval_grantedOn":"%s",' % app['grantedOn']
                 if 'email' in app['by']:
                     bulk_json_ap += '"approval_email":"%s",' % app['by']['email']
+                    bulk_json_ap += '"approval_organization":"%s",' % get_organization(app['by']['email'])
                 else:
-                    bulk_json_ap += '"approval_email":None,'
+                    bulk_json_ap += '"approval_email":null,'
+                    bulk_json_ap += '"approval_organization":null,'
                 if 'username' in app['by']:
                     bulk_json_ap += '"approval_username":"%s"' % app['by']['username']
                 else:
-                    bulk_json_ap += '"approval_username":None'
+                    bulk_json_ap += '"approval_username":null'
                 bulk_json_event = '{%s,%s,%s}' % (bulk_json_review,
                                                   bulk_json_patch, bulk_json_ap)
 
@@ -133,6 +288,7 @@ def fetch_events(review):
                 app_count += 1
 
     url = elasticsearch_url+'/gerrit/reviews_events/_bulk'
+    url = elasticsearch_url+'/gerrit_openstack/reviews_events/_bulk'
     request = urllib2.Request(url, data=bulk_json)
     request.get_method = lambda: 'POST'
 
@@ -164,6 +320,7 @@ def project_reviews_to_es(project):
     # Parse JSON document
     gerrit_json = json.loads(raw_data)
 
+    elasticsearch_type = "reviews"
     for item in gerrit_json:
         if 'project' in item.keys():
             # Detected review JSON object
@@ -172,6 +329,7 @@ def project_reviews_to_es(project):
 
             data_json = json.dumps(item)
             url = elasticsearch_url + "/"+elasticsearch_index
+            url += "/"+elasticsearch_type
             url += "/"+str(item["id"])
             request = urllib2.Request(url, data=data_json)
             request.get_method = lambda: 'PUT'
@@ -179,26 +337,118 @@ def project_reviews_to_es(project):
 
             fetch_events(item)
 
+def sortinghat_to_es():
+    """ Load all identities data in SH """
+
+    elasticsearch_type = "profiles"
+    url = elasticsearch_url + "/"+elasticsearch_index
+    url_type = url + "/" + elasticsearch_type
+
+    profile_map = """
+    {
+        "properties": {
+           "email": {
+              "type": "string",
+              "index":"not_analyzed"
+           },
+           "uuid": {
+              "type": "string"
+           },
+           "is_bot": {
+              "type": "boolean"
+           },
+           "organization": {
+              "type": "string",
+              "index":"not_analyzed"
+           }
+        }
+    }
+    """
+
+    # Create mappings
+    url_map = url_type+"/_mapping"
+    request = urllib2.Request(url_map, data=profile_map)
+    request.get_method = lambda: 'PUT'
+    opener.open(request)
+
+    sortinghat_db = "acs_sortinghat_mediawiki_5879"
+    sortinghat_db = "amartin_sortinghat_openstack_sh"
+    db = Database (user = "root", passwd = "",
+                   host = "localhost", port = 3306,
+                   scrdb = None, shdb = sortinghat_db, prjdb = None)
+
+    sql = """
+        SELECT p.uuid, email, is_bot, o.name as organization
+        FROM profiles p
+        JOIN enrollments e ON e.uuid=p.uuid
+        JOIN organizations o ON o.id = e.organization_id
+        WHERE email is not NULL
+        """
+    profiles = db.execute(sql)
+
+    # bulk_json = ""# To Load in ES quickly
+    profiles_count = 0
+    profiles_error = 0
+
+
+    for profile in profiles:
+        uuid = profile[0]
+        email = profile[1]
+        is_bot = profile[2]
+        organization = profile[3]
+
+        email2org[email] = organization
+
+        if email is None: continue
+        profile_json = "{"
+        profile_json += '"uuid":"%s",' % uuid
+        profile_json += '"email":"%s",' % email
+        profile_json += '"is_bot":"%s",' % is_bot
+        profile_json += '"organization":"%s"' % organization
+        profile_json += "}"
+
+        url_profile = url_type + "/"+str(uuid)
+        request = urllib2.Request(url_profile, data=profile_json)
+        request.get_method = lambda: 'PUT'
+        try:
+            opener.open(request)
+            profiles_count += 1
+        except:
+            logging.error("Can't add: " + email)
+            profiles_error += 1
+
+#         bulk_json += '{"index" : {"_id" : "%s" } }\n' % (uuid)  # Bulk operation
+#         bulk_json += profile_json +"\n"  # Bulk document
+
+
+    print "Profiles loaded: %i" % profiles_count
+    print "Profiles error: %i" % profiles_error
+
 if __name__ == '__main__':
 
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
     cache_dir = "cache"
     if not os.path.exists(cache_dir):
-
         os.makedirs(cache_dir)
-    # Get reviews JSON data from gerrit using SSH
 
-    max_items = "500"
-
+    # Reviews will be added to ES using its API REST
     elasticsearch_url = "http://sega.bitergia.net:9200"
     elasticsearch_url = "http://localhost:9200"
-    elasticsearch_index = "gerrit/reviews"
-    gerrit_cmd  = "ssh -p 29418 acs@gerrit.wikimedia.org "
-    gerrit_cmd += "gerrit "
+    elasticsearch_index = "gerrit"
+    elasticsearch_index = "gerrit_openstack"
 
+    # Get reviews JSON data from gerrit using SSH
+    max_items = "500"
+    gerrit_cmd  = "ssh -p 29418 acs@gerrit.wikimedia.org "
+    gerrit_cmd  = "ssh -p 29418 acs@review.openstack.org "
+    gerrit_cmd += "gerrit "
     # Feed the reviews items in EL
     opener = urllib2.build_opener(urllib2.HTTPHandler)
+
+    # Add profiles to sortinghat
+    email2org = {}
+    sortinghat_to_es()
 
     # First we need all projects
     projects = get_projects()
