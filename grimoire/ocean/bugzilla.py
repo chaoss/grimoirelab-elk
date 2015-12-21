77241a29b7c323593495758568dbbe1b7323f8d2
Alvaro del Castillo <acs@bitergia.com>
2015-12-21 08:24:30 +0100

    [bugzilla ocean] Implemented get_identities.

--- grimoire/ocean/bugzilla.py
+++ grimoire/ocean/bugzilla.py
@@ -29,6 +29,50 @@ from grimoire.ocean.elastic import ElasticOcean
 
 class BugzillaOcean(ElasticOcean):
 
+    def get_identities(self, item):
+        ''' Return the identities from an item '''
+
+        def add_identity(identities, user):
+            identity = {}
+            for field in ['name', 'email', 'username']:
+                identity[field] = None
+            if 'name' in user: identity['name'] = user['name']
+            if 'email' in user: identity['email'] = user['email']
+            if 'username' in user: identity['username'] = user['username']
+            if 'assigned_to_name' in user:
+                    identity['name'] = user['assigned_to_name']
+                    identity['username'] = user['assigned_to']
+            elif 'assigned_to' in user: identity['username'] = user['assigned_to']
+            if 'changed_by' in user: identity['name'] = user['changed_by']
+            if 'who' in user: identity['username'] = user['who']
+            if 'reporter' in user:
+                    identity['name'] = user['reporter_name']
+                    identity['username'] = user['reporter']
+            identities.append(identity)
+
+        identities = []
+
+        if 'changes' in item:
+            for change in item['changes']:
+                add_identity(identities, change)
+        if 'long_desc' in item:
+            for comment in item['long_desc']:
+                add_identity(identities, comment)
+        if 'assigned_to_name' in item:
+            add_identity(identities, {'assigned_to_name': item['assigned_to_name'],
+                                      'assigned_to':item['assigned_to']})
+        elif 'assigned_to' in item:
+            add_identity(identities, {'assigned_to': item['assigned_to']})
+        if 'reporter_name' in item:
+            add_identity(identities, {'reporter_name': item['reporter_name'],
+                                      'reporter':item['reporter']})
+        if 'reporter_name' in item:
+            add_identity(identities, {'reporter_name': item['reporter_name'],
+                                      'reporter':item['reporter']})
+
+        return identities
+
+
     def get_field_date(self):
         field = None
         if self.perceval_backend.detail == "list":
