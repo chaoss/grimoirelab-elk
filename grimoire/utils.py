d60bb19b01090383c67f6a493474769e50e90396
Alvaro del Castillo <acs@bitergia.com>
2016-07-26 17:02:16 +0200

    [grimoire] Add Twitter support

--- grimoire/utils.py
+++ grimoire/utils.py
@@ -46,6 +46,7 @@ from grimoire.ocean.remo import ReMoOcean
 from grimoire.ocean.stackexchange import StackExchangeOcean
 from grimoire.ocean.supybot import SupybotOcean
 from grimoire.ocean.telegram import TelegramOcean
+from grimoire.ocean.twitter import TwitterOcean
 
 
 # Connectors for EnrichOcean
@@ -64,7 +65,7 @@ from grimoire.elk.remo import ReMoEnrich
 from grimoire.elk.stackexchange import StackExchangeEnrich
 from grimoire.elk.supybot import SupybotEnrich
 from grimoire.elk.telegram import TelegramEnrich
-
+from grimoire.elk.twitter import TwitterEnrich
 
 # Connectors for Perceval
 from perceval.backends.bugzilla import Bugzilla, BugzillaCommand
@@ -128,7 +129,8 @@ def get_connectors():
             "stackexchange":[StackExchange, StackExchangeOcean,
                              StackExchangeEnrich, StackExchangeCommand],
              "supybot":[Supybot, SupybotOcean, SupybotEnrich, SupybotCommand],
-             "telegram":[Telegram, TelegramOcean, TelegramEnrich, TelegramCommand]
+             "telegram":[Telegram, TelegramOcean, TelegramEnrich, TelegramCommand],
+             "twitter":[None, TwitterOcean, TwitterEnrich, None]
             }  # Will come from Registry
 
 def get_elastic(url, es_index, clean = None, ocean_backend = None):
