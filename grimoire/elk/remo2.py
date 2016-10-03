0e8c23fd4260a73a4cb9a5df864391ae7f1b726c
Alvaro del Castillo <acs@bitergia.com>
2016-10-03 17:23:05 +0200

    [enrich][remo] Add support for activities enrichment

--- grimoire/elk/remo2.py
+++ grimoire/elk/remo2.py
@@ -57,7 +57,11 @@ class ReMoEnrich(Enrich):
                   },
                 "geolocation": {
                    "type": "geo_point"
-                }
+                },
+                "functional_areas": {
+                  "type": "string",
+                  "index":"analyzed"
+                  }
            }
         } """
 
@@ -68,7 +72,12 @@ class ReMoEnrich(Enrich):
 
         identities = []
         item = item['data']
-        identities.append(self.get_sh_identity(item['owner']))
+        if 'owner' in item:
+            identities.append(self.get_sh_identity(item['owner']))
+        if 'user' in item:
+            identities.append(self.get_sh_identity(item['user']))
+        if 'mentor' in item:
+            identities.append(self.get_sh_identity(item['mentor']))
 
         return identities
 
@@ -87,16 +96,103 @@ class ReMoEnrich(Enrich):
 
         return identity
 
+    def get_item_category(self, item):
+        # We need to detect the category of item: activities (report), events or users
+        category = None
+
+        item = item['data']
+
+        if 'estimated_attendance' in item:
+            category = 'events'
+        elif 'activity' in item:
+            category = 'activities'
+        elif 'first_name' in item:
+            category = 'users'
+        else:
+            logging.error("Can not detect category in item %s", item)
+
+        return category
+
     def get_rich_item(self, item):
+        # We need to detect the category of item: activities (report), events or users
         eitem = {}
 
+        eitem.update(self.__get_rich_item_common(item))
+
+        category = self.get_item_category(item)
+
+        if category == 'activities':
+            eitem.update(self.__get_rich_item_activities(item))
+        elif category == 'users':
+            eitem.update(self.__get_rich_item_users(item))
+        elif category == 'events':
+            eitem.update(self.__get_rich_item_events(item))
+
+        return eitem
+
+    def __get_rich_item_common(self, item):
         # metadata fields to copy
+
+        eitem = {}
+
         copy_fields = ["metadata__updated_on","metadata__timestamp","ocean-unique-id","origin","offset"]
         for f in copy_fields:
             if f in item:
                 eitem[f] = item[f]
             else:
                 eitem[f] = None
+
+        return eitem
+
+
+    def __get_rich_item_activities(self, item):
+        eitem = {}
+
+        # The real data
+        activity = item['data']
+
+
+        # data fields to copy
+        copy_fields = ["initiative","location","activity","link",
+                       "activity_description","remo_url",
+                       "external_link","latitude","longitude", "report_date"]
+        for f in copy_fields:
+            if f in activity:
+                eitem[f] = activity[f]
+            else:
+                eitem[f] = None
+
+
+        eitem['user_profile_url'] = activity['user']['_url']
+        eitem['user'] = activity['user']['first_name']+" "+activity['user']['last_name']
+
+        if 'mentor' in activity and activity['mentor']:
+            eitem['mentor_profile_url'] = activity['mentor']['_url']
+            eitem['mentor'] = activity['mentor']['first_name']+" "+activity['mentor']['last_name']
+
+        # geolocation
+        eitem['geolocation'] = {
+            "lat": eitem['latitude'],
+            "lon": eitem['longitude'],
+        }
+
+        eitem['functional_areas'] = ''
+        for area in eitem['functional_areas']:
+            eitem['functional_areas'] += "," + area['name']
+
+        if self.sortinghat:
+            eitem.update(self.get_item_sh(item, "user"))
+
+        return eitem
+
+    def __get_rich_item_users(self, item):
+        eitem = {}
+
+        return eitem
+
+    def __get_rich_item_events(self, item):
+        eitem = {}
+
         # The real data
         event = item['data']
 
