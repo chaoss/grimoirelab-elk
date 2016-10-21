1151316bf5c7484dbb6315c3e86166455b7739e4
Alvaro del Castillo <acs@bitergia.com>
2016-10-21 20:00:16 +0200

    [enrich][utils] New method to convert from unixtime to datetime using timezones

--- grimoire/elk/utils.py
+++ grimoire/elk/utils.py
@@ -24,7 +24,7 @@
 #
 
 import datetime
-from dateutil import parser
+from dateutil import parser, tz
 
 def get_time_diff_days(start, end):
     ''' Number of days between two dates in UTC format  '''
@@ -43,3 +43,18 @@ def get_time_diff_days(start, end):
     diff_days = float('%.2f' % diff_days)
 
     return diff_days
+
+# https://github.com/grimoirelab/perceval/blob/master/perceval/utils.py#L149
+def unixtime_to_datetime(ut):
+    """Convert a unixtime timestamp to a datetime object.
+    The function converts a timestamp in Unix format to a
+    datetime object. UTC timezone will also be set.
+    :param ut: Unix timestamp to convert
+    :returns: a datetime object
+    :raises InvalidDateError: when the given timestamp cannot be
+        converted into a valid date
+    """
+
+    dt = datetime.datetime.utcfromtimestamp(ut)
+    dt = dt.replace(tzinfo=tz.tzutc())
+    return dt
