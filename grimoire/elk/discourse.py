757e4abd13590065a7d3b814aa30365b407ab19b
Alvaro del Castillo <acs@bitergia.com>
2016-12-19 14:19:04 +0100

    Implement comments enrich and sh

--- grimoire/elk/discourse.py
+++ grimoire/elk/discourse.py
@@ -37,36 +37,68 @@ class DiscourseEnrich(Enrich):
         """ Return the identities from an item """
         identities = []
 
-        participants = item['data']['details']['participants']
+        # All identities are in the post stream
+        # The first post is the question. Next replies
 
-        for identity in participants:
-            user = self.get_sh_identity(identity)
+        posts = item['data']['post_stream']['posts']
+
+        for post in posts:
+            user = self.get_sh_identity(post)
             identities.append(user)
         return identities
 
-    def get_sh_identity(self, item, identity_field=None):
+    def get_sh_identity(self, post, identity_field=None):
         identity = {}
 
-        user = item  # by default a specific user dict is expected
-        if 'data' in item and type(item) == dict:
-            user = item['data']['details'][identity_field]
-        identity['username'] = user['username']
+        if identity_field in post:
+            post = post[identity_field]
+
+        identity['username'] = post['username']
         identity['email'] = None
-        identity['name'] = user['username']
+        identity['name'] = post['display_username']
         return identity
 
     def get_field_author(self):
-        return 'created_by'
+        return 'author'
 
-    def get_users_data(self, item):
-        """ If user fields are inside the global item dict """
-        if 'data' in item:
-            users_data = item['data']['details']
-        else:
-            # the item is directly the data (kitsune answer)
-            users_data = item
+    def get_users_data(self, post):
+        """ Adapt the data to be used with standard SH enrich API """
+        poster = {}
+        poster[self.get_field_author()] = post
+        return poster
 
-        return users_data
+    def get_rich_item_answers(self, item):
+        answers_enrich = []
+        nanswers = 0
+
+        for answer in item['data']['post_stream']['posts']:
+            eanswer = self.get_rich_item(item)  # reuse all fields from item
+            eanswer['id'] = answer['id']
+            eanswer['url'] = eanswer['origin'] + "/t/" + answer['topic_slug']
+            eanswer['url'] += "/" + str(answer['topic_id']) + "/" + str(answer['post_number'])
+            eanswer['type'] = 'answer'
+            eanswer.update(self.get_grimoire_fields(answer['created_at'], eanswer['type']))
+            eanswer['display_username'] = answer['display_username']
+            eanswer['username'] = answer['username']
+            eanswer['author_id'] = answer['user_id']
+            eanswer['reads'] = answer['reads']
+            eanswer['reply_count'] = answer['reply_count']
+            eanswer['time_from_question'] = None
+            post_time = answer['created_at']
+            item_time = item['data']['created_at']
+            eanswer['first_reply_time'] = get_time_diff_days(item_time, post_time)
+            answers_enrich.append(eanswer)
+
+            if self.sortinghat:
+                print(self.get_item_sh(answer, date_field="created_at"))
+                eanswer.update(self.get_item_sh(answer, date_field="created_at"))
+
+            nanswers += 1
+            eanswer['first_answer'] = 0
+            if nanswers == 1:
+                eanswer['first_answer'] = 1
+
+        return answers_enrich
 
     @metadata
     def get_rich_item(self, item):
@@ -110,27 +142,48 @@ class DiscourseEnrich(Enrich):
         if 'question_replies' in eitem:
             eitem['question_replies'] -= 1  # index enrich spec
 
-        # The first post is the first published
-        posts = topic['post_stream']['posts'][0]
-        eitem['url'] = eitem['origin'] + "/t/" + posts['topic_slug']
-        eitem['url'] += "/" + str(posts['topic_id']) + "/" + str(posts['post_number'])
-        eitem['author_user_name'] = posts['display_username']
-        eitem['author_id'] = posts['user_id']
-        eitem['reads'] = posts['reads']
-        eitem['reply_count'] = posts['reply_count']
+        # The first post is the first published, and it is the question
+        first_post = topic['post_stream']['posts'][0]
+        eitem['url'] = eitem['origin'] + "/t/" + first_post['topic_slug']
+        eitem['url'] += "/" + str(first_post['topic_id']) + "/" + str(first_post['post_number'])
+        eitem['display_username'] = first_post['display_username']
+        eitem['username'] = first_post['username']
+        eitem['author_id'] = first_post['user_id']
+        eitem['reads'] = first_post['reads']
+        eitem['reply_count'] = first_post['reply_count']
 
         # First reply time
         eitem['time_from_question'] = None
         firt_post_time = None
         if len(topic['post_stream']['posts'])>1:
-            firt_post_time = topic['post_stream']['posts'][0]['created_at']
+            firt_post_time = first_post['created_at']
             second_post_time = topic['post_stream']['posts'][1]['created_at']
             eitem['first_reply_time'] = get_time_diff_days(firt_post_time, second_post_time)
 
         if self.sortinghat:
-            eitem.update(self.get_item_sh(item))
+            print(self.get_item_sh(first_post, date_field="created_at"))
+            eitem.update(self.get_item_sh(first_post, date_field="created_at"))
 
         eitem['type'] = 'question'
         eitem.update(self.get_grimoire_fields(topic["created_at"], eitem['type']))
 
         return eitem
+
+    def get_field_unique_id_answer(self):
+        return "id"
+
+    def enrich_items(self, items):
+        # nitems = super(DiscourseEnrich, self).enrich_items(items)
+        # logging.info("Total questions enriched: %i", nitems)
+
+        # And now for each item we want also the answers (tops)
+        nanswers = 0
+        rich_item_answers = []
+
+        for item in items:
+            rich_item_answers += self.get_rich_item_answers(item)
+
+        if rich_item_answers:
+            nanswers += self.elastic.bulk_upload(rich_item_answers,
+                                                 self.get_field_unique_id_answer())
+        logging.info("Total answers enriched: %i", nanswers)
