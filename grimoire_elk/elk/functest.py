9a2a250e9a5cca6de6f85b812499805f2cbcd99a
Alvaro del Castillo <acs@bitergia.com>
2017-07-03 18:50:54 +0200

    [enrich][functest] Add support for enriching FuncTest data source coming from OPNFV

--- grimoire_elk/elk/functest.py
+++ grimoire_elk/elk/functest.py
@@ -32,58 +32,13 @@ from .enrich import Enrich, metadata
 logger = logging.getLogger(__name__)
 
 
-
 class FunctestEnrich(Enrich):
-
-    def get_field_author(self):
-        return "user_data"
-
-    def get_elastic_mappings(self):
-
-        mapping = """
-        {
-            "properties": {
-                "text_analyzed": {
-                  "type": "string",
-                  "index":"analyzed"
-                  }
-           }
-        } """
-
-        return {"items":mapping}
-
-    def get_sh_identity(self, item, identity_field=None):
-        identity = {
-            'username': None,
-            'name': None,
-            'email': None
-        }
-
-        from_ = item
-        if 'data' in item and type(item) == dict:
-            if self.get_field_author() not in item['data']:
-                # Message from bot
-                identity['username'] = item['data']['bot_id']
-                return identity
-            from_ = item['data'][self.get_field_author()]
-
-        identity['username'] = from_['name']
-        identity['name'] = from_['real_name']
-        if 'profile' in from_:
-            if 'email' in from_['profile']:
-                identity['email'] = from_['profile']['email']
-        return identity
 
     def get_identities(self, item):
         """ Return the identities from an item """
         identities = []
 
-        identity = self.get_sh_identity(item)
-
-        identities.append(identity)
-
         return identities
-
 
     @metadata
     def get_rich_item(self, item):
@@ -96,95 +51,32 @@ class FunctestEnrich(Enrich):
                 eitem[f] = None
 
         # The real data
-        message = item['data']
+        func_test = item['data']
 
         # data fields to copy
-        copy_fields = ["text", "type", "reply_count", "subscribed", "subtype",
-                       "unread_count", "user"]
+        copy_fields = ["start_date", "stop_date", "case_name", "criteria", "scenario",
+                       "version", "pod_name", "installer", "build_tag", "trust_indicator"]
         for f in copy_fields:
-            if f in message:
-                eitem[f] = message[f]
+            if f in func_test:
+                eitem[f] = func_test[f]
             else:
                 eitem[f] = None
 
-        eitem['text_analyzed'] = eitem['text']
+        # Fields which names are translated
+        map_fields = {"_id": "api_id",
+                      "project_name": "project"
+                     }
+        for fn in map_fields:
+            if fn in func_test:
+                eitem[map_fields[fn]] = func_test[fn]
+            else:
+                eitem[map_fields[fn]] = None
 
-        eitem['number_attachs'] = 0
-        if 'attachments' in message:
-            eitem['number_attachs'] = len(message['attachments'])
 
-        eitem['reaction_count'] = 0
-        if 'reactions' in message:
-            eitem['reaction_count'] = len(message['reactions'])
-            eitem['reactions'] = []
-            for rdata in message['reactions']:
-                # {
-                #         "count": 2,
-                #         "users": [
-                #            "U38J51N7J",
-                #            "U3Q0VLHU3"
-                #         ],
-                #         "name": "+1"
-                # }
-                for i in range(0, rdata['count']):
-                    eitem['reactions'].append(rdata["name"])
-
-        if 'file' in message:
-            eitem['file_type'] = message['file']['pretty_type']
-            eitem['file_title'] = message['file']['title']
-            eitem['file_size'] = message['file']['size']
-            eitem['file_name'] = message['file']['name']
-            eitem['file_mode'] = message['file']['mode']
-            eitem['file_is_public'] = message['file']['is_public']
-            eitem['file_is_external'] = message['file']['is_external']
-            eitem['file_id'] = message['file']['id']
-            eitem['file_is_editable'] = message['file']['editable']
-
-        if 'user_data' in message:
-            eitem['team_id'] = message['user_data']['team_id']
-            eitem['tz'] = message['user_data']['tz_offset']
-            # tz must be in -12h to 12h interval, so seconds -> hours
-            eitem['tz'] = round(int(eitem['tz'])/(60*60))
-            eitem['is_admin'] = message['user_data']['is_admin']
-            eitem['is_owner'] = message['user_data']['is_owner']
-            eitem['is_primary_owner'] = message['user_data']['is_primary_owner']
-            if 'profile' in message['user_data']:
-                if 'title' in message['user_data']['profile']:
-                    eitem['profile_title'] = message['user_data']['profile']['title']
-                eitem['avatar'] = message['user_data']['profile']['image_32']
-
-        if self.sortinghat:
-            eitem.update(self.get_item_sh(item))
-
-        eitem.update(self.get_grimoire_fields(item["metadata__updated_on"], "message"))
-
-        # Channel info
-        channel = message['channel_info']
-        eitem['channel_name'] = channel['name']
-        eitem['channel_id'] = channel['id']
-        eitem['channel_created'] = channel['created']
-        eitem['channel_member_count'] = len(channel['members'])
-        if 'topic' in channel:
-            eitem['channel_topic'] = channel['topic']
-        if 'purpose' in channel:
-            eitem['channel_purpose'] = channel['purpose']
-        channel_bool_fields = ['is_archived', 'is_general', 'is_starred']
-        for field in channel_bool_fields:
-            eitem['channel_' + field] = 0
-            if field in channel and channel[field]:
-                eitem['channel_' + field] = 1
-
-        eitem = self.__convert_booleans(eitem)
+        if 'details' in func_test:
+            if 'tests' in func_test['details']:
+                eitem['tests'] = func_test['details']['tests']
+            if 'failures' in func_test['details']:
+                eitem['failures'] = func_test['details']['failures']
 
         return eitem
-
-    def __convert_booleans(self, eitem):
-        """ Convert True/False to 1/0 for better kibana processing """
-
-        for field in eitem.keys():
-            if isinstance(eitem[field], bool):
-                if eitem[field]:
-                    eitem[field] = 1
-                else:
-                    eitem[field] = 0
-        return eitem  # not needed becasue we are modifying directly the dict
