2b2bbc01fdca74f22629fb44f2fdfcabbfb4a176
Alvaro del Castillo <acs@bitergia.com>
2017-07-31 08:10:07 +0200

    [ocean] Read the items from arthur and not from perceval. First version.

--- grimoire_elk/ocean/elastic.py
+++ grimoire_elk/ocean/elastic.py
@@ -127,8 +127,7 @@ class ElasticOcean(ElasticItems):
         # Also add timestamp used in incremental enrichment
         item['metadata__timestamp'] = timestamp.isoformat()
 
-    def feed(self, from_date=None, from_offset=None, category=None,
-             latest_items=None):
+    def feed(self, from_date=None, from_offset=None, category=None, latest_items=None, arthur_items=None):
         """ Feed data in Elastic from Perceval """
 
         if from_date and from_offset:
@@ -169,6 +168,8 @@ class ElasticOcean(ElasticItems):
         items_pack = []  # to feed item in packs
         drop = 0
         added = 0
+
+
         if self.fetch_cache:
             items = self.perceval_backend.fetch_from_cache()
         else:
@@ -197,6 +198,9 @@ class ElasticOcean(ElasticItems):
                     items = self.perceval_backend.fetch(category=category)
                 else:
                     items = self.perceval_backend.fetch()
+
+        if arthur_items:
+            items = arthur_items
 
         for item in items:
             # print("%s %s" % (item['url'], item['lastUpdated_date']))
