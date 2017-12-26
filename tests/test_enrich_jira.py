258de3f0f678ed28ee565897221a8459f96b99e4
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2017-12-27 00:31:50 +0100

    [Jira] Move enrichement of fields property to a classmethod.
    To be able of testing the code, we need to move it to a classmmethod.
    This allows to call it without instantiating an object, which
    is problematic when testing. This patch does that, and adds some
    tests to check specifically this new method (enrich_fields).

---
+++ tests/test_enrich_jira.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Jesus M. Gonzalez-Barahona <jgb@bitergia.com>
+#
+
+import json
+import logging
+import os.path
+import sys
+import unittest
+
+if '..' not in sys.path:
+    sys.path.insert(0, '..')
+
+from grimoire_elk.elk.jira import JiraEnrich
+
+class TestEnrichJira(unittest.TestCase):
+    """Functional unit tests for GrimoireELK Enrichment for Jira"""
+
+    def setUp(self):
+        self.ritems = []
+        self.eitems = []
+        with open(os.path.join("data", "jira_raw_fields.json")) as f:
+            for line in f:
+                self.ritems.append(json.loads(line))
+        with open(os.path.join("data", "jira_enriched_fields.json")) as f:
+            for line in f:
+                self.eitems.append(json.loads(line))
+
+    def test_enrich_fields(self):
+        """Test enrich_fields function"""
+
+        for ritem, eitem in zip(self.ritems, self.eitems):
+            enriched = {}
+            if 'fields' in ritem['_source']['data']:
+                JiraEnrich.enrich_fields(ritem['_source']['data']['fields'], enriched)
+            self.assertDictEqual(enriched, eitem)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+    unittest.main()
