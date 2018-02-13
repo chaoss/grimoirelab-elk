1eea4f2071218f5ca5c37a4223307038dd44bbf1
Valerio Cosentino <valcos@bitergia.com>
2018-02-13 09:59:12 +0100

    [ocean] Fix mappings to index Redmine data
    This patch prevents to index subjects and descriptions of Redmine
    items, which may cause failures when uploading the data to ES

--- grimoire_elk/ocean/redmine.py
+++ grimoire_elk/ocean/redmine.py
@@ -40,21 +40,54 @@ class Mapping(BaseMapping):
         :returns:        dictionary with a key, 'items', with the mapping
         """
 
-        mapping = '''
-         {
-            "dynamic":true,
-                "properties": {
-                    "data": {
-                        "properties": {
-                            "journals": {
-                                "dynamic":false,
-                                "properties": {}
+        if es_major != 2:
+            mapping = '''
+             {
+                "dynamic":true,
+                    "properties": {
+                        "data": {
+                            "properties": {
+                                "journals": {
+                                    "dynamic":false,
+                                    "properties": {}
+                                },
+                                "description": {
+                                    "type": "text",
+                                    "index": true
+                                },
+                                "subject": {
+                                    "type": "text",
+                                    "index": true
+                                }
                             }
                         }
                     }
+            }
+            '''
+        else:
+            mapping = '''
+                 {
+                    "dynamic":true,
+                        "properties": {
+                            "data": {
+                                "properties": {
+                                    "journals": {
+                                        "dynamic":false,
+                                        "properties": {}
+                                    },
+                                    "description": {
+                                        "type": "string",
+                                        "index": "analyzed
+                                    },
+                                    "subject": {
+                                        "type": "string",
+                                        "index": "analyzed
+                                    }
+                                }
+                            }
+                        }
                 }
-        }
-        '''
+                '''
 
         return {"items": mapping}
 
