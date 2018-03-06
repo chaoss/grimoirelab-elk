561e8dffef448d0fd042803f8f708e9edd5c2314
Valerio Cosentino <valcos@bitergia.com>
2018-03-06 20:37:43 +0100

    [tests] Add tests for kitsune
    This code includes tests for Kitsune

---
+++ tests/test_kitsune.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2016 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Alvaro del Castillo <acs@bitergia.com>
+#     Valerio Cosentino <valcos@bitergia.com>
+#
+
+import logging
+import unittest
+
+from base import TestBaseBackend
+
+
+class TestKitsune(TestBaseBackend):
+    """Test Kitsune backend"""
+
+    connector = "kitsune"
+    ocean_index = "test_" + connector
+    enrich_index = "test_" + connector + "_enrich"
+
+    def test_items_to_raw(self):
+        """Test whether JSON items are properly inserted into ES"""
+
+        result = self._test_items_to_raw()
+        self.assertEqual(result['items'], 4)
+        self.assertEqual(result['raw'], 4)
+
+    def test_raw_to_enrich(self):
+        """Test whether the raw index is properly enriched"""
+
+        result = self._test_raw_to_enrich()
+        self.assertEqual(result['raw'], 4)
+        self.assertEqual(result['enrich'], 9)
+
+    def test_raw_to_enrich_sorting_hat(self):
+        """Test enrich with SortingHat"""
+
+        result = self._test_raw_to_enrich(sortinghat=True)
+        self.assertEqual(result['raw'], 4)
+        self.assertEqual(result['enrich'], 9)
+
+    def test_raw_to_enrich_projects(self):
+        """Test enrich with Projects"""
+
+        result = self._test_raw_to_enrich(projects=True)
+        self.assertEqual(result['raw'], 4)
+        self.assertEqual(result['enrich'], 9)
+
+    def test_refresh_identities(self):
+        """Test refresh identities"""
+
+        result = self._test_refresh_identities()
+        # ... ?
+
+    def test_refresh_project(self):
+        """Test refresh project field for all sources"""
+
+        result = self._test_refresh_project()
+        # ... ?
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+    logging.getLogger("requests").setLevel(logging.WARNING)
+    unittest.main(warnings='ignore')
