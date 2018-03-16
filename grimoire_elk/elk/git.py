964c838a4ab034be68ae75e3699f68b2380fec0b
Valerio Cosentino <valcos@bitergia.com>
2018-03-16 17:48:37 +0100

    [elk] Limit content size of message attribute for Git data
    This patch limits the content size of the message attribute to
    32000, thus avoiding max_bytes_length_exceeded_exception

--- grimoire_elk/elk/git.py
+++ grimoire_elk/elk/git.py
@@ -334,6 +334,7 @@ class GitEnrich(Enrich):
                 eitem[map_fields[fn]] = commit[fn]
             else:
                 eitem[map_fields[fn]] = None
+        eitem['message'] = commit['message'][:self.KEYWORD_MAX_SIZE]
         eitem['hash_short'] = eitem['hash'][0:6]
         # Enrich dates
         author_date = parser.parse(commit["AuthorDate"])
