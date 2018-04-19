c4c6147c2f53b47055857977df364eac0b1fb799
Valerio Cosentino <valcos@bitergia.com>
2018-04-19 16:54:24 +0200

    [phabricator] Handle long description.raw value in raw items
    This patch prevents to index description.raw attributes of
    phabricator items, thus avoiding immense term exception in ES.
    Description.raw attribute is not used neither in the enriched item
    nor in the corresponding panels (according to the csv in the
    schema folder).

--- grimoire_elk/raw/phabricator.py
+++ grimoire_elk/raw/phabricator.py
@@ -56,6 +56,10 @@ class Mapping(BaseMapping):
                                     "properties": {
                                         "subpriority" : {"type": "float"}
                                      }
+                                 },
+                                 "description": {
+                                    "dynamic":false,
+                                    "properties": {}
                                  }
                              }
                          }
