7a17883e2935951147174eee607049d6016739af
Valerio Cosentino <valcos@bitergia.com>
2018-04-26 17:21:23 +0200

    [dockerhub] Remove ES 2.x mappings
    This code removes the mappings for raw and enriched indexes.

--- grimoire_elk/raw/dockerhub.py
+++ grimoire_elk/raw/dockerhub.py
@@ -37,46 +37,25 @@ class Mapping(BaseMapping):
         :returns:        dictionary with a key, 'items', with the mapping
         """
 
-        if es_major != '2':
-            mapping = '''
-             {
-                "dynamic":true,
-                    "properties": {
-                        "data": {
-                            "properties": {
-                                "description": {
-                                    "type": "text",
-                                    "index": true
-                                },
-                                "full_description": {
-                                    "type": "text",
-                                    "index": true
-                                }
+        mapping = '''
+         {
+            "dynamic":true,
+                "properties": {
+                    "data": {
+                        "properties": {
+                            "description": {
+                                "type": "text",
+                                "index": true
+                            },
+                            "full_description": {
+                                "type": "text",
+                                "index": true
                             }
                         }
                     }
-            }
-            '''
-        else:
-            mapping = '''
-             {
-                "dynamic":true,
-                    "properties": {
-                        "data": {
-                            "properties": {
-                                "description": {
-                                    "type": "string",
-                                    "index": "analyzed"
-                                },
-                                "full_description": {
-                                    "type": "string",
-                                    "index": "analyzed"
-                                }
-                            }
-                        }
-                    }
-            }
-            '''
+                }
+        }
+        '''
 
         return {"items": mapping}
 
