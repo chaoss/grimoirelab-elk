cba992cd52c003b2fc2841c92a85c1a9269e4376
Valerio Cosentino <valcos@bitergia.com>
2018-04-26 17:21:23 +0200

    [dockerhub] Remove ES 2.x mappings
    This code removes the mappings for raw and enriched indexes.

--- grimoire_elk/enriched/dockerhub.py
+++ grimoire_elk/enriched/dockerhub.py
@@ -43,44 +43,24 @@ class Mapping(BaseMapping):
         :returns:        dictionary with a key, 'items', with the mapping
         """
 
-        if es_major != '2':
-            mapping = """
-            {
-                "properties": {
-                    "description": {
-                        "type": "text",
-                        "index": true
-                    },
-                    "description_analyzed": {
-                        "type": "text",
-                        "index": true
-                    },
-                    "full_description_analyzed": {
-                        "type": "text",
-                        "index": true
-                    }
-               }
-            }
-            """
-        else:
-            mapping = """
-            {
-                "properties": {
-                    "description": {
-                        "type": "string",
-                        "index": "analyzed"
-                    },
-                    "description_analyzed": {
-                        "type": "string",
-                        "index": "analyzed"
-                    },
-                    "full_description_analyzed": {
-                        "type": "string",
-                        "index": "analyzed"
-                    }
-               }
-            }
-            """
+        mapping = """
+        {
+            "properties": {
+                "description": {
+                    "type": "text",
+                    "index": true
+                },
+                "description_analyzed": {
+                    "type": "text",
+                    "index": true
+                },
+                "full_description_analyzed": {
+                    "type": "text",
+                    "index": true
+                }
+           }
+        }
+        """
 
         return {"items": mapping}
 
