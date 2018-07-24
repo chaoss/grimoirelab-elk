301c13b04bdc88637dc94f06692ccf257c9342c7
Valerio Cosentino <valcos@bitergia.com>
2018-07-24 10:53:37 +0200

    [enriched] Change shebang line

--- grimoire_elk/enriched/bugzillarest.py
+++ grimoire_elk/enriched/bugzillarest.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #
 # BugzillaREST to Elastic class helper
--- grimoire_elk/enriched/database.py
+++ grimoire_elk/enriched/database.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #
 # Basic class for getting data from MySQL databases
