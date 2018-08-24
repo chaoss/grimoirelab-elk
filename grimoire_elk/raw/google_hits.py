1067bd07c80db93c329ce9737028e4b22a4a2309
Valerio Cosentino <valcos@bitergia.com>
2018-08-24 17:39:28 +0200

    [raw] Add support for googlehits data
    This code enables ELK to handle googlehits raw data coming
    from the Perceval backend.

---
+++ grimoire_elk/raw/google_hits.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# StackExchange Ocean feeder
+#
+# Copyright (C) 2015 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#   Valerio Cosentino <valcos@bitergia.com>
+#
+
+"""GoogleHits Ocean feeder"""
+
+from .elastic import ElasticOcean
+
+
+class GoogleHitsOcean(ElasticOcean):
+    """GoogleHits Ocean feeder"""
+
+    @classmethod
+    def get_arthur_params_from_url(cls, url):
+        """ Get the arthur params given a URL for the data source """
+        return url
