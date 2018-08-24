1d72bb73b98a0fa3de77171b6f723587304818c1
Valerio Cosentino <valcos@bitergia.com>
2018-08-24 17:41:52 +0200

    [tests] Add tests for googlehits connectors
    This code includes test data to check that the googlehits
    connectors properly work.

---
+++ tests/test_google_hits.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2016 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Valerio Cosentino <valcos@bitergia.com>
+#
+import json
+import logging
+import unittest
+
+from base import TestBaseBackend
+from grimoire_elk.raw.google_hits import GoogleHitsOcean
+
+
+class TestGoogleHits(TestBaseBackend):
+    """Test GoogleHits backend"""
+
+    connector = "google_hits"
+    ocean_index = "test_" + connector
+    enrich_index = "test_" + connector + "_enrich"
+
+    def test_items_to_raw(self):
+        """Test whether JSON items are properly inserted into ES"""
+
+        result = self._test_items_to_raw()
+
+        self.assertGreater(result['items'], 0)
+        self.assertGreater(result['raw'], 0)
+        self.assertEqual(result['items'], result['raw'])
+
+    def test_raw_to_enrich(self):
+        """Test whether the raw index is properly enriched"""
+
+        result = self._test_raw_to_enrich()
+
+        self.assertGreater(result['raw'], 0)
+        self.assertGreater(result['enrich'], 0)
+        self.assertEqual(result['raw'], result['enrich'])
+
+    def test_raw_to_enrich_projects(self):
+        """Test enrich with Projects"""
+
+        result = self._test_raw_to_enrich(projects=True)
+        # ... ?
+
+    def test_refresh_project(self):
+        """Test refresh project field for all sources"""
+
+        result = self._test_refresh_project()
+        # ... ?
+
+    def test_arthur_params(self):
+        """Test the extraction of arthur params from an URL"""
+
+        with open("data/projects-release.json") as projects_filename:
+            url = json.load(projects_filename)['grimoire']['google_hits'][0]
+            arthur_params = 'bitergia grimoirelab'
+            self.assertEqual(arthur_params, GoogleHitsOcean.get_arthur_params_from_url(url))
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+    logging.getLogger("requests").setLevel(logging.WARNING)
+    unittest.main(warnings='ignore')
