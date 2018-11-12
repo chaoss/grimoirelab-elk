1c859e695bce2e972e154c50dc690d18c6954ae4
Valerio Cosentino <valcos@bitergia.com>
2018-11-12 09:53:43 +0100

    [kitsune] Replace string mappings with dict
    This code replaces the string-based mappings definition to its
    equivalent dict. Furthermore, it aligns the logic of enrich_items
    to make it compatible with the new changes in the elasticsearch
    class.

--- grimoire_elk/enriched/kitsune.py
+++ grimoire_elk/enriched/kitsune.py
@@ -21,7 +21,6 @@
 #   Alvaro del Castillo San Felix <acs@bitergia.com>
 #
 
-import json
 import logging
 
 from dateutil import parser
@@ -44,17 +43,16 @@ class Mapping(BaseMapping):
         :returns:        dictionary with a key, 'items', with the mapping
         """
 
-        mapping = """
-        {
+        mapping = {
             "properties": {
                 "content_analyzed": {
-                  "type": "text"
-                  },
+                    "type": "text"
+                },
                 "tags_analyzed": {
-                  "type": "text"
-                  }
-           }
-        } """
+                    "type": "text"
+                }
+            }
+        }
 
         return {"items": mapping}
 
@@ -211,28 +209,17 @@ class KitsuneEnrich(Enrich):
 
     def enrich_items(self, ocean_backend):
         max_items = self.elastic.max_items_bulk
-        current = 0
-        bulk_json = ""
+        to_insert = []
         total = 0
 
-        url = self.elastic.index_url + '/items/_bulk'
-
-        logger.debug("Adding items to %s (in %i packs)", url, max_items)
+        logger.debug("Adding items to %s (in %i packs)", self.elastic.index_url, max_items)
 
         items = ocean_backend.fetch()
         for item in items:
-            if current >= max_items:
-                total += self.elastic.safe_put_bulk(url, bulk_json)
-                bulk_json = ""
-                current = 0
-
             rich_item = self.get_rich_item(item)
-            data_json = json.dumps(rich_item)
-            bulk_json += '{"index" : {"_id" : "%s" } }\n' % \
-                (item[self.get_field_unique_id()])
-            bulk_json += data_json + "\n"  # Bulk document
-            current += 1
-            # Time to enrich also de answers
+            es_item = self.elastic.es_data_format(rich_item, self.get_field_unique_id())
+            to_insert.append(es_item)
+
             if 'answers_data' in item['data']:
                 for answer in item['data']['answers_data']:
                     # Add question title in answers
@@ -241,14 +228,15 @@ class KitsuneEnrich(Enrich):
                     if answer['id'] == item['data']['solution']:
                         answer['solution'] = 1
                     rich_answer = self.get_rich_item(answer, kind='answer')
-                    data_json = json.dumps(rich_answer)
-                    bulk_json += '{"index" : {"_id" : "%s_%i" } }\n' % \
-                        (item[self.get_field_unique_id()],
-                         rich_answer['answer_id'])
-                    bulk_json += data_json + "\n"  # Bulk document
-                    current += 1
+                    es_item = self.elastic.es_data_format(rich_item, self.get_field_unique_id())
+                    es_item['_id'] = es_item['_id'] + '_' + str(rich_answer['answer_id'])
+                    to_insert.append(es_item)
 
-        if current > 0:
-            total += self.elastic.safe_put_bulk(url, bulk_json)
+            if len(to_insert) >= max_items:
+                total += self.elastic.bulk(to_insert)
+                to_insert = []
+
+        if len(to_insert) > 0:
+            total += self.elastic.bulk(to_insert)
 
         return total
