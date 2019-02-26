6d5aa8822f62e36a9f453db408493a90e031fb82
Valerio Cosentino <valcos@bitergia.com>
2019-02-26 11:25:52 +0100

    [raw] Handle filter raw for jira
    This code allows to parse the filter raw information from the entries within the
    mordred projects.json. The projects.json should be changed to the following way:
    `jira-url --filter-raw=data.fields.project.key:TARGET-PROJECT-KEY`

--- grimoire_elk/raw/jira.py
+++ grimoire_elk/raw/jira.py
@@ -85,7 +85,23 @@ class JiraOcean(ElasticOcean):
     @classmethod
     def get_arthur_params_from_url(cls, url):
         """ Get the arthur params given a URL for the data source """
-        return {"url": url}
+
+        tokens = url.split(' ', 1)
+
+        return {"url": tokens[0]}
+
+    @classmethod
+    def get_p2o_params_from_url(cls, url):
+        params = {}
+
+        tokens = url.split(' ', 1)
+        params['url'] = tokens[0]
+
+        if len(tokens) > 1:
+            f = tokens[1].split("=")[1]
+            params['filter-raw'] = f
+
+        return params
 
     def _fix_item(self, item):
         # Remove all custom fields to avoid the 1000 fields limit in ES
