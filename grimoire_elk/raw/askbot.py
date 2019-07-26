3ce862fe3881e3f6886006217b25635e1bcbab9b
Valerio Cosentino <valcos@bitergia.com>
2019-07-26 23:41:35 +0200

    [raw-askbot] Change mapping
    This code changes the mapping of the raw items.
    The new mapping is the following:
    ```
        {
         "dynamic":true,
         "properties": {
            "data": {
               "dynamic":false,
               "properties": {}
           }
          }
        }
    ```
    
    Signed-off-by: Valerio Cosentino <valcos@bitergia.com>

--- grimoire_elk/raw/askbot.py
+++ grimoire_elk/raw/askbot.py
@@ -29,34 +29,17 @@ class Mapping(BaseMapping):
         """Get Elasticsearch mapping.
 
         :param es_major: major version of Elasticsearch, as string
-        :returns:        dictionary with a key, 'items', with the mapping
+        :returns: dictionary with a key, 'items', with the mapping
         """
-
         mapping = '''
          {
             "dynamic":true,
-                "properties": {
-                    "data": {
-                        "properties": {
-                            "answers": {
-                                "dynamic":false,
-                                "properties": {}
-                            },
-                            "author": {
-                                "dynamic":false,
-                                "properties": {}
-                            },
-                            "comments": {
-                                "dynamic":false,
-                                "properties": {}
-                            },
-                            "summary": {
-                                "type": "text",
-                                "index": true
-                            }
-                        }
-                    }
+            "properties": {
+                "data": {
+                    "dynamic":false,
+                    "properties": {}
                 }
+            }
         }
         '''
 
