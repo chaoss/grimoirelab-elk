256e4ccad94b5e8bafc12a0966b379f452e23863
Valerio Cosentino <valcos@bitergia.com>
2019-07-26 23:42:47 +0200

    [raw-bugzillarest] Change mapping
    This code changes the mapping of the raw items.
    The new mapping is the following:
    ```
        {
         "dynamic":true,
         "properties": {
            "data": {
               "dynamic":false,
               "properties": {}
           }
          }
        }
    ```
    
    Signed-off-by: Valerio Cosentino <valcos@bitergia.com>

--- grimoire_elk/raw/bugzillarest.py
+++ grimoire_elk/raw/bugzillarest.py
@@ -32,79 +32,17 @@ class Mapping(BaseMapping):
         """Get Elasticsearch mapping.
 
         :param es_major: major version of Elasticsearch, as string
-        :returns:        dictionary with a key, 'items', with the mapping
+        :returns: dictionary with a key, 'items', with the mapping
         """
-
         mapping = '''
          {
             "dynamic":true,
-                "properties": {
-                    "data": {
-                        "properties": {
-                            "comments": {
-                                "dynamic":false,
-                                "properties": {
-                                    "raw_text": {
-                                        "type": "text",
-                                        "index": true
-                                    },
-                                    "text": {
-                                        "type": "text",
-                                        "index": true
-                                    }
-                                }
-                            },
-                            "attachments": {
-                                "properties": {
-                                    "description" : {
-                                        "type": "text",
-                                        "index": true
-                                    },
-                                    "summary" : {
-                                        "type": "text",
-                                        "index": true
-                                    }
-                                }
-                            },
-                            "clones": {
-                                "dynamic":false,
-                                "properties": {}
-                            },
-                            "description": {
-                                "type": "text",
-                                "index": true
-                            },
-                            "history": {
-                                "properties": {
-                                    "changes": {
-                                        "properties": {
-                                            "removed": {
-                                                "type": "text",
-                                                "index": true
-                                            },
-                                            "added": {
-                                                "type": "text",
-                                                "index": true
-                                            },
-                                            "comment": {
-                                                "properties": {
-                                                    "thetext": {
-                                                        "type": "text",
-                                                        "index": true
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            },
-                            "summary": {
-                                "type": "text",
-                                "index": true
-                            }
-                        }
-                    }
+            "properties": {
+                "data": {
+                    "dynamic":false,
+                    "properties": {}
                 }
+            }
         }
         '''
 
