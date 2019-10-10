87fec03e4017e43b77fe3fbfe22d12bfdad68c3e
Valerio Cosentino <valcos@bitergia.com>
2019-10-10 19:06:16 +0200

    [tests] Add test case for errors class
    Signed-off-by: Valerio Cosentino <valcos@bitergia.com>

---
+++ tests/test_errors.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2019 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#     Valerio Cosentino <valcos@bitergia.com>
+#
+
+import unittest
+
+import grimoire_elk.errors as errors
+
+
+# Mock classes to test BaseError class
+class MockErrorNoArgs(errors.BaseError):
+    message = "Mock error without args"
+
+
+class MockErrorArgs(errors.BaseError):
+    message = "Mock error with args. Error: %(code)s %(msg)s"
+
+
+class TestBaseError(unittest.TestCase):
+
+    def test_subblass_with_no_args(self):
+        """Check subclasses that do not require arguments.
+
+        Arguments passed to the constructor should be ignored.
+        """
+        e = MockErrorNoArgs(code=1, msg='Fatal error')
+
+        self.assertEqual("Mock error without args", str(e))
+
+    def test_subclass_args(self):
+        """Check subclasses that require arguments"""
+
+        e = MockErrorArgs(code=1, msg='Fatal error')
+
+        self.assertEqual("Mock error with args. Error: 1 Fatal error",
+                         str(e))
+
+    def test_subclass_invalid_args(self):
+        """Check when required arguments are not given.
+
+        When this happens, it raises a KeyError exception.
+        """
+        kwargs = {'code': 1, 'error': 'Fatal error'}
+        self.assertRaises(KeyError, MockErrorArgs, **kwargs)
+
+
+class TestELKError(unittest.TestCase):
+
+    def test_message(self):
+        """Make sure that prints the correct error"""
+
+        e = errors.ELKError(cause='key error on item')
+        self.assertEqual('key error on item', str(e))
+
+
+if __name__ == "__main__":
+    unittest.main()
