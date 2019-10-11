7fa284643845b5f9c05d0030bceef7b26ca24b48
Valerio Cosentino <valcos@bitergia.com>
2019-10-11 14:09:51 +0200

    [tests] Increase test coverage for raw supybot
    Signed-off-by: Valerio Cosentino <valcos@bitergia.com>

--- tests/test_supybot.py
+++ tests/test_supybot.py
@@ -19,7 +19,6 @@
 #     Alvaro del Castillo <acs@bitergia.com>
 #     Valerio Cosentino <valcos@bitergia.com>
 #
-import json
 import logging
 import unittest
 
@@ -91,14 +90,25 @@ class TestSupybot(TestBaseBackend):
         result = self._test_refresh_project()
         # ... ?
 
+    def test_perceval_params(self):
+        """Test the extraction of perceval params from an URL"""
+
+        url = "openshift /home/bitergia/.perceval/irc/percevalbot/logs/ChannelLogger/freenode/#openshift/"
+        expected_params = [
+            'openshift',
+            '/home/bitergia/.perceval/irc/percevalbot/logs/ChannelLogger/freenode/#openshift/'
+        ]
+        self.assertListEqual(SupybotOcean.get_perceval_params_from_url(url), expected_params)
+
     def test_arthur_params(self):
         """Test the extraction of arthur params from an URL"""
 
-        with open("data/projects-release.json") as projects_filename:
-            url = json.load(projects_filename)['grimoire']['supybot'][0]
-            arthur_params = {'dirpath': '/home/bitergia/.perceval/irc/percevalbot/logs/ChannelLogger/freenode/#openshift/',
-                             'uri': 'openshift'}
-            self.assertDictEqual(arthur_params, SupybotOcean.get_arthur_params_from_url(url))
+        url = "openshift /home/bitergia/.perceval/irc/percevalbot/logs/ChannelLogger/freenode/#openshift/"
+        expected_params = {
+            'dirpath': '/home/bitergia/.perceval/irc/percevalbot/logs/ChannelLogger/freenode/#openshift/',
+            'uri': 'openshift'
+        }
+        self.assertDictEqual(SupybotOcean.get_arthur_params_from_url(url), expected_params)
 
 
 if __name__ == "__main__":
