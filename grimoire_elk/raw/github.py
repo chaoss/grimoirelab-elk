f0e03e04bccd4da9ee4654364c56dbd85668f9d0
JJMerchante <jj.merchante@gmail.com>
2020-02-08 14:22:27 +0100

    [github] Fix filter classified in GitHub and GitHub2 enrich
    This code checks if user_data, assignee_data, merged_by_data, comments_data.user_data and
    review_comments_data.user_data is in the raw item.
    In case don't exist, define default values for those fields.
    
    Signed-off-by: JJMerchante <jj.merchante@gmail.com>

--- grimoire_elk/raw/github.py
+++ grimoire_elk/raw/github.py
@@ -110,3 +110,46 @@ class GitHubOcean(ElasticOcean):
         params.append(dparam['owner'])
         params.append(dparam['repository'])
         return params
+
+    def _fix_item(self, item):
+        category = item['category']
+
+        if 'classified_fields_filtered' not in item or not item['classified_fields_filtered']:
+            return
+
+        item = item['data']
+        comments_attr = None
+        if category == "issue":
+            identity_types = ['user', 'assignee']
+            comments_attr = 'comments_data'
+        elif category == "pull_request":
+            identity_types = ['user', 'merged_by']
+            comments_attr = 'review_comments_data'
+        else:
+            identity_types = []
+
+        for identity in identity_types:
+            if identity not in item:
+                continue
+            if not item[identity]:
+                continue
+
+            identity_attr = identity + "_data"
+
+            item[identity_attr] = {
+                'name': item[identity]['login'],
+                'login': item[identity]['login'],
+                'email': None,
+                'company': None,
+                'location': None,
+            }
+
+        comments = item.get(comments_attr, [])
+        for comment in comments:
+            comment['user_data'] = {
+                'name': comment['user']['login'],
+                'login': comment['user']['login'],
+                'email': None,
+                'company': None,
+                'location': None,
+            }
