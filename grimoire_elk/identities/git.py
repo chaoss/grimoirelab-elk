0361c38c03367f07247e5989378bc8698a6adca9
JJMerchante <jj.merchante@gmail.com>
2020-03-26 13:57:09 +0100

    [git] Make author anonymization optional for git
    This code makes it optional to anonymize the author and committer
    information. It creates a hash of the name and only keeps the domain
     of the email. For enabling the anonymization, include
    the following parameter in the git section:
    ```
    [git]
    ...
    anonymize = true
    ```
    
    Signed-off-by: JJMerchante <jj.merchante@gmail.com>

---
+++ grimoire_elk/identities/git.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2020 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#   Jose Javier Merchante Picazo <jjmerchante@gcauldron.io>
+#
+
+from grimoire_elk.identities.identities import Identities
+
+
+class GitIdentities(Identities):
+
+    @staticmethod
+    def _get_identity(git_user):
+        identity = {}
+        fields = git_user.split("<")
+        identity['name'] = fields[0].strip()
+        try:
+            email = fields[1][:-1]
+            identity['domain'] = email.split("@")[1]
+        except IndexError:
+            identity['domain'] = 'unknown'
+
+        return identity
+
+    @classmethod
+    def anonymize_item(cls, item):
+        """Remove or hash the fields that contain personal information"""
+
+        item = item['data']
+
+        if item['Author']:
+            author = cls._get_identity(item['Author'])
+            item['Author'] = "{} <xxxxxx@{}>".format(cls._hash(author['name']), author['domain'])
+        if item['Commit']:
+            commit = cls._get_identity(item['Commit'])
+            item['Commit'] = "{} <xxxxxx@{}>".format(cls._hash(commit['name']), commit['domain'])
