GeorgLink/GrimoireELK

[raw-jira] Change mapping
This code changes the mapping of the Jira raw items.
The new mapping is the following:
```
{
 "dynamic":true,
 "properties": {
    "data": {
       "dynamic":false,
       "properties": {}
   }
  }
}
```

Signed-off-by: Valerio Cosentino <valcos@bitergia.com>

tests/test_jira.py
@@ -153,7 +153,7 @@ class TestJira(TestBaseBackend):
 
         with open("data/projects-release.json") as projects_filename:
             url = json.load(projects_filename)['grimoire']['jira'][0]
-            p2o_params = {'url': 'https://jira.opnfv.org', 'filter-raw': 'data.fields.project.key:PROJECT-KEY'}
+            p2o_params = {'url': 'https://jira.opnfv.org', 'filter-raw': 'search_terms.project_key:PROJECT-KEY'}
             self.assertDictEqual(p2o_params, JiraOcean.get_p2o_params_from_url(url))
 
 

chaoss/grimoirelab-elk

tests/test_jira.py
@@ -185,7 +185,7 @@ class TestJira(TestBaseBackend):
 
         with open("data/projects-release.json") as projects_filename:
             url = json.load(projects_filename)['grimoire']['jira'][0]
-            p2o_params = {'url': 'https://jira.opnfv.org', 'filter-raw': 'data.fields.project.key:PROJECT-KEY'}
+            p2o_params = {'url': 'https://jira.opnfv.org', 'filter-raw': 'search_fields.project_key:PROJECT-KEY'}
             self.assertDictEqual(p2o_params, JiraOcean.get_p2o_params_from_url(url))
 
 