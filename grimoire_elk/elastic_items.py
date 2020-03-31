GeorgLink/GrimoireELK

[elastic_items] Change filter-raw parameter
This code changes the filter-raw parameter to
`search_terms`. This change is needed to
use the new Perceval metadata attribute
`search_terms` to filter raw data.

Signed-off-by: Valerio Cosentino <valcos@bitergia.com>

grimoire_elk/elastic_items.py
@@ -32,7 +32,7 @@ from .elastic_mapping import Mapping
 HEADER_JSON = {"Content-Type": "application/json"}
 MAX_BULK_UPDATE_SIZE = 1000
 
-FILTER_DATA_ATTR = 'data.'
+FILTER_DATA_ATTR = 'search_terms.'
 FILTER_SEPARATOR = r",\s*%s" % FILTER_DATA_ATTR
 PROJECTS_JSON_LABELS_PATTERN = r".*(--labels=\[(.*)\]).*"
 

chaoss/grimoirelab-elk

grimoire_elk/elastic_items.py
@@ -33,7 +33,7 @@ from .elastic_mapping import Mapping
 HEADER_JSON = {"Content-Type": "application/json"}
 MAX_BULK_UPDATE_SIZE = 1000
 
-FILTER_DATA_ATTR = 'data.'
+FILTER_DATA_ATTR = 'search_fields.'
 FILTER_SEPARATOR = r",\s*%s" % FILTER_DATA_ATTR
 PROJECTS_JSON_LABELS_PATTERN = r".*(--labels=\[(.*)\]).*"
 