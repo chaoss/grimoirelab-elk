677a46026a27b92a7a545f3bf8f0eebf01571583
Valerio Cosentino <valcos@bitergia.com>
2020-05-09 19:00:45 +0200

    [enriched-utils] Remove credentials for repository filter
    This code omits the credentials that may appear on the
    repo origin (e.g., git private repos) when creating
    the filter to get the data from the corresponding
    raw index.
    
    Signed-off-by: Valerio Cosentino <valcos@bitergia.com>

--- grimoire_elk/enriched/utils.py
+++ grimoire_elk/enriched/utils.py
@@ -54,7 +54,7 @@ def get_repository_filter(perceval_backend, perceval_backend_name, term=False):
         return filter_
 
     field = 'origin'
-    value = perceval_backend.origin
+    value = anonymize_url(perceval_backend.origin)
 
     if perceval_backend_name in ["meetup", "nntp", "stackexchange", "jira"]:
         # Until tag is supported in all raw and enriched indexes
