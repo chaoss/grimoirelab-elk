0653a8918b5ed7330279361a66cd624d860a3133
Jose Javier Merchante <jjmerchante@bitergia.com>
2023-01-09 11:07:48 +0100

    Update Copyright notice to 2023
    This commit updates the Copyright notes in Python files to be
    2023.
    
    Signed-off-by: Jose Javier Merchante <jjmerchante@bitergia.com>

--- grimoire_elk/identities/stackexchange.py
+++ grimoire_elk/identities/stackexchange.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2015-2020 Bitergia
+# Copyright (C) 2015-2023 Bitergia
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
